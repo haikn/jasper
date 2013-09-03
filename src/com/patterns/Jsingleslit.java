1f81f41744aa59bb3c42c2fea5daad34912e099b
Admin <Admin@PC2013041622NZL>
2013-09-03 17:27:57 +0800

    update

--- src/com/patterns/Jsingleslit.java
+++ src/com/patterns/Jsingleslit.java
@@ -28,7 +28,7 @@ public class Jsingleslit extends javax.swing.JFrame {
 
         jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jButton1.setText("jButton1");
 
