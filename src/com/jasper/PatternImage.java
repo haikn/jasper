d2dccbd59d287a130e1ff0fe6c2c47544a4fdf18
T420 <T420@T420-PC>
2013-10-22 13:32:40 +0700

    Delete PhaseRetader

--- src/com/jasper/PatternImage.java
+++ src/com/jasper/PatternImage.java
@@ -1180,21 +1180,20 @@ public class PatternImage {
 
         return resizedImage;
     }
-
-    private static BufferedImage resizeImageWithHint(BufferedImage originalImage, int type) {
-
-        BufferedImage resizedImage = new BufferedImage(1920, 1080, type);
-        Graphics2D g = resizedImage.createGraphics();
-        g.drawImage(originalImage, 0, 0, 1920, 1080, null);
-        g.dispose();
-        g.setComposite(AlphaComposite.Src);
-
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g.setRenderingHint(RenderingHints.KEY_RENDERING,
-                RenderingHints.VALUE_RENDER_QUALITY);
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
-        return resizedImage;
-    }
+//    private static BufferedImage resizeImageWithHint(BufferedImage originalImage, int type) {
+//
+//        BufferedImage resizedImage = new BufferedImage(1920, 1080, type);
+//        Graphics2D g = resizedImage.createGraphics();
+//        g.drawImage(originalImage, 0, 0, 1920, 1080, null);
+//        g.dispose();
+//        g.setComposite(AlphaComposite.Src);
+//
+//        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+//                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+//        g.setRenderingHint(RenderingHints.KEY_RENDERING,
+//                RenderingHints.VALUE_RENDER_QUALITY);
+//        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+//                RenderingHints.VALUE_ANTIALIAS_ON);
+//        return resizedImage;
+//    }
 }
