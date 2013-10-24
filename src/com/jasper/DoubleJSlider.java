6d95a5c32c0144de16bac6f6014474d90754608b
sonnv <>
2013-10-24 17:49:28 +0700

    add DoubleJSlider  class

---
+++ src/com/jasper/DoubleJSlider.java
@@ -0,0 +1,30 @@
+/*
+ * @(#)DoubleJSlider.java
+ *
+ * Copyright (c) 2013 JASPER DISPLAY, Inc.
+ * An Unpublished Work.  All Rights Reserved.
+ *
+ * JASPER DISPLAY PROPRIETARY:  Distribution of this source code
+ * without permission from the copyright holder is strictly forbidden.
+ */
+package com.jasper;
+
+import javax.swing.JSlider;
+
+/**
+ *
+ * @author sonnv
+ */
+public class DoubleJSlider extends JSlider {
+
+    final int scale;
+
+    public DoubleJSlider(int min, int max, int value, int scale) {
+        super(min, max, value);
+        this.scale = scale;
+    }
+
+    public double getScaledValue() {
+        return ((double)super.getValue()) / this.scale;
+    }
+}
