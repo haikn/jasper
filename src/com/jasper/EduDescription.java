4887153314739c2328b7ecc04af656c305a5c16e
sonnv <>
2013-11-15 09:49:01 +0700

    added description tab

---
+++ src/com/jasper/EduDescription.java
@@ -0,0 +1,159 @@
+/*
+ * @(#)EduDescription.java
+ *
+ * Copyright (c) 2013 JASPER DISPLAY, Inc.
+ * An Unpublished Work.  All Rights Reserved.
+ *
+ * JASPER DISPLAY PROPRIETARY:  Distribution of this source code
+ * without permission from the copyright holder is strictly forbidden.
+ */
+package com.jasper;
+
+import javax.swing.JLabel;
+
+/**
+ * This EduDescription include the description information of application
+ *
+ * @version 1.0 28 Aug 2013
+ *
+ * @author sonnv
+ *
+ */
+public class EduDescription {
+    public static javax.swing.JLabel desTelephotoLens =
+            new JLabel("<html><div style=\"padding-left:5px;padding-top:5px;margin: 0 0.07em 0 -0.13em;background-color:white;width:100%;word-spacing:30px;font-family:MS Mincho;font-size:11px;font-weight: normal;\">" +
+            "&nbsp; &nbsp; &nbsp; LCOS-SLM (Liquid Crystal on Silicon- Spatial Light Modulator)是 能 調 制<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "光 振 幅 及 相 位 的 空 間 光 調 制 器，其 內 部 結 構 如 同 一 般 的 液 晶 顯<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "示 器，是 由 許 多 格 狀 的 像 素 所 組 成 的 矩 陣 結 構，當 施 加 電 壓 時 每<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "一 個 像 素 上 的 液 晶 會 依 電 壓 大  小 有 不 同 角 度 的 偏 轉 造 成 不 同<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "的 折 射 率 而 達 到 調 制 的 效 果。 在 未 給 予 訊 號 的 情 形 下 SLM 基 本<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "的 結 構 就 會 影 響 到 光 的 表 現，包 括 最 大 繞 射 角 度、繞 射 效 率、及<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "偏 振 態 的 變 化，利 用 這 些 特 性 我 們 便 能 推 算 出 SLM 各 項 基 本 參<br /><div style=\"width:100%;height:3px;\"></div>"
+            + " 數，如 像 素 大 小、開  口 率、及 配 向 角  度。<br /><div style=\"width:100%;height:25px;\"></div>"
+            + "<br></div></html>");
+    public static javax.swing.JLabel desMicroscope =
+            new JLabel("<html><div style=\"padding-left:6px;padding-top:5px;margin: 0 0.07em 0 -0.13em;background-color:white;width:100%;word-spacing:30px;font-family:MS Mincho;font-size:11px;font-weight: normal;\">" +
+            "&nbsp; &nbsp; &nbsp;SLM 配 合 偏 振 片(Polarizer)與 檢 偏 片(Analyzer)的 角 度 調 整 能 使 SLM<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "變 成 振 幅 調 制 器(Amplitude Modulator)或 是 相 位 調 制 器 (Phase Modulator)<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "在 此 實 驗 中 我 們 調 整 偏 振 片 及 檢 偏 片 的 角 度 使 其 與 配 向 角 夾<div style=\"width:100%;height:3px;\"></div>"
+            + "四 十 五 度，在 此 設 定 下 SLM 為 振 幅 調 制 器，整 組 光 學 系 統 就 是 簡<div style=\"width:100%;height:3px;\"></div>"
+            + "單 的 投 影 系 統，也 利 用 此 系 統 了 解 成 像 系 統 中 的 各 項 參 數 如: <div style=\"width:100%;height:3px;\"></div>"
+            + "f-number, Aperture Stop, Entrance Pupil, Exit pupil..等。 <br /><div style=\"width:100%;height:80px;\"></div>"
+            + "</div></html>");
+    public static javax.swing.JLabel desAberration =
+            new JLabel("<html><div style=\"padding-left:6px;padding-top:5px;margin: 0 0.07em 0 -0.13em;background-color:white;width:100%;word-spacing:30px;font-family:MS Mincho;font-size:11px;font-weight: normal;\">" +
+            "&nbsp; &nbsp; &nbsp; 在 實 驗 二 裡，我 們 使 偏 振 片 及 檢 偏 片 的 角 度 與 配 向 角 夾 四<div style=\"width:100%;height:3px;\"></div>"
+            + "十 五 度 做 為 振 幅 調 制 器。在 相 位 調 制 上，我 們 這 次 改 變 偏 振 片 及<div style=\"width:100%;height:3px;\"></div>"
+            + "檢 偏 片 的 角 度 使 其 與 配 向 角 平 行 做 為 相 位 調 制 器，接 著 我 們 利<div style=\"width:100%;height:3px;\"></div>"
+            + "用 傾 斜 式 光 柵(Blazed Grating)讓 相 位 調 制 器 做 簡 單 的 光 束 控 制<div style=\"width:100%;height:3px;\"></div>"
+            + "(Beam Steering) 實 驗 來 了 解 繞 射 角 度 的 極 限。 <div style=\"width:100%;height:100px;\"></div>"
+            + "</html>");
+    public static javax.swing.JLabel desMichelson =
+            new JLabel("<html><div style=\"padding-left:5px;padding-top:5px;margin: 0 0.07em 0 -0.13em;background-color:white;width:100%;word-spacing:30px;font-family:MS Mincho;font-size:11px;font-weight: normal;\">" +
+            "&nbsp; &nbsp; &nbsp; 干 涉 的 現 象 是 由 兩 個 或 兩 個 以 上 的 光 波 疊 加 於 空 間 之 中<br /><div style=\"width:100%;height:3px;\"></div> 而 產 生。由 光 程 差 所\n" +
+            "造 成 的 相 位 差 而 產 生 亮 紋（建 設 性 干 涉）與<br /><div style=\"width:100%;height:3px;\"></div> 暗 紋（破 壞 性 干 涉）等 干 涉 條 紋。要\n" +
+            "觀 察 到 干 涉 條 紋 除 了 需 要 上 <br /><div style=\"width:100%;height:3px;\"></div> 述 的 相 位 差 所 造 成 的 建 設 性 干 涉 與 破 壞 性 干 涉 之 外,\n" +
+            "還 需 要<br /><div style=\"width:100%;height:3px;\"></div>注 意 光 波 偏 振 方 向 與 相 干 性（coherence，簡 而 言 之，兩 個 有 相 同 <br /><div style=\"width:100%;height:3px;\"></div>\n" +
+            "偏 振 的 光 波 到 達 屏 幕 的 光 程 差越 接 近 的 話，干 涉 條 紋 的 對 比 也<br /><div style=\"width:100%;height:3px;\"></div> 會 越 好。" +
+            "<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "&nbsp; &nbsp; &nbsp; 本 實 驗 中 的 干 涉 儀 利 用 了 分 光 鏡 將 入 射 的 雷 射 光 分 成 兩<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "道 光 ，一 道 穿 透 光 與 一 道 反 射 光，這 種 分 光 的 原 理 被 分 類 為 振<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "幅 分 光 (division of amplitude) 和 楊 格 雙 狹 縫 干 涉 中 利 用 相 鄰 的 兩<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "條 狹 縫 把 同 一 道 波 前 分 成 兩 道 新 的 光 源 的 波 前 分 光(division of<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "wave front) 不 同 。 接 著 由 分 光 所 分 出 的 穿 透 光 經 由 相 位 調 制 器<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "產 生 各 種 波 前 與 反 射 光 經 由 面 鏡 反 射 回 來 在 空 間 中 交 會 產 生<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "干 涉， 並 能 在 屏 幕 上 觀 察 到 干 涉 圖 形。<br /><div style=\"width:100%;height:3px;\"></div>" +
+            "</div></html>");
+    public static javax.swing.JLabel desDiffaction =
+            new JLabel("<html><div style=\"padding-left:5px;padding-top:5px;margin: 0 0.07em 0 -0.13em;background-color:white;width:100%;word-spacing:30px;font-family:MS Mincho;font-size:11px;font-weight: normal;\">"
+            + "&nbsp; &nbsp; &nbsp; 光 的 波 動 性 質 可 以 很 容 易 地 從 干 涉 與 繞 射 的 現 象 中 觀 察<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "到，要 解 釋 其 原 因 得 先 從 <strong>Huygens-Fresnel</strong> 原 理 說 起，其 內 容 為 波<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "前 上 每 一 點 都可 視 為 新 的 點 波 源 各 自 發 出 次 級 球 面 波，波 前 的<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "新 位 置 則 是 這 些 球 面 的 包 絡 面。因 此 在 單 狹 縫 繞 射 實 驗 中，平<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "面 波 遇 上 狹 縫 後，狹 縫 上 的 每 一 點 都 視 為 新 的 點 光 源 在 各 方<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "向 發 出 光 波，直 進 的 非 繞 射 光 在 遠 處 以 相 同 的 相 位 到 達 屏 幕<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "形 成 亮 點。現 在 有 另 一 束 光 以 與 水 平 夾 角 θ 的 方 向 行 進，當 光<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "束 最 上 層 與 最 下 層 的 光 程 差 剛 好 為 一 個波長時， 正中 間 的 光 束<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "會 與 最 上 面 的 光 束 相 差 了 半 個 波 長 (相 位 相 差 ö)而 相 消，接 著<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "往 下 推 下 去 整 束 光 都 會 相 差 半 波 長 而 相 消 掉，最 後 在 屏 幕 上<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "顯 示 暗 紋。依 相 同 的 原 理 可 推 得 當 光 程 差 為 波 長 整 數 倍 時 即<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "會 產  生 暗 紋。 楊 格 的 雙 狹 縫 實 驗 為 波 前 分 光 的 干 涉 儀,原 波<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "前 被 兩 條 狹 縫 擋 住 只 留 下 通 過 狹 縫 的 部 份，通 過 狹 縫 的 光 被<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "視 為 新 的 點 波 源 在 遠 處 屏 幕 上 形 成 干 涉 條 紋， 兩 個 點 波源 會<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "往 四 面 八 方 散 出 光 線，當 射 出 光 線 與 水 平 夾 角 為 ，光 程 差 為<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "一 個 波 長 時，兩 束 為 建 設 性 干 涉，最 後 在 遠 處 屏 幕 形 成 亮 紋 。<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "此 結 果 與 單 狹 縫 不 同，同 樣 在 最 上 方 與 最 下 方 的 光 源 處 相 差<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "整 數 倍 波 長，但 產 生 的 結 果 為 一 個 暗 紋 一 個 亮 紋，至 於 雙 狹 縫<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "的 暗 紋 則 會 在 光 程 差 等 於 半 波 長 造 成 破 壞 性 干 涉 時 產 生。<br /><br />"
+            + ""
+            + "</div></html>");
+    public static javax.swing.JLabel desSpectrometer =
+            new JLabel("<html><div style=\"padding-left:5px;padding-top:5px;margin: 0 0.07em 0 -0.13em;background-color:white;width:100%;word-spacing:30px;font-family:MS Mincho;font-size:11px;font-weight: normal;\">"
+            + "&nbsp; &nbsp; &nbsp; 光 譜 儀 常 被 應 用 於 各 種 材 料 的 光 特 性 分 析 上， 這 是 由 於<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "光 譜 儀 可 以 分 析 光 譜 並 量 測 各 波 長 的 強 度 分 布。 其 原 理 為 利<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "用 不 同 頻 率 的 光 對 同 一 周 期 性 光 柵 會 有 不 同 的 繞 射 角 度， 假<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "設 光 柵 周 期 為 d、 波 長 為 λ、 繞 射 角 度 為 θ，當 周 期 性 的 光 柵<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "與 波 長 關 係 如 下 時<br /><div style=\"width:100%;height:6px;\"></div>"
+            + "<div style=\"font-size:16px;font-weight:bold;\">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; dsinθm = m </div> "
+            + "<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "其 中 m 為 繞 射 階 數， 此 時 與 原 光 路 夾 θm 的 方 向 便 有 繞 射 光 產<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "生。隨 著 波 長 的 不 同 所 造 成 的 繞 射 角 度 也 不 同 ， 結 果 如 下 圖，<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "檢 測 光 入 射 至 光 柵 裡 受 周 期 性 光 柵 的 影 響 不 同 波 長 的 光 繞 射<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "至 不 同 方 向，紅 色 由 於 波 長 較 長 所 以 角 度 也 會 跟 著 變 大， 藍 色<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "由 於 波 長 較 短 所 以 角 度 會 比 較 接 近 原 反 射 路 線， 接 著 由 透 鏡<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "將 不 同 波 長 的 光 聚 焦 在 焦 平 面 上， 並 量 測 各 波 段 位 置、角 度 來<br /><div style=\"width:100%;height:3px;\"></div>"
+            + " 推 算 其 波 長。"
+            + "</div></html>");
+    
+    public static javax.swing.JLabel desSignalProcessing =
+            new JLabel("<html><div style=\"padding-left:6px;padding-top:5px;margin: 0 0.07em 0 -0.13em;background-color:white;width:100%;word-spacing:30px;font-family:MS Mincho;font-size:11px;font-weight: normal;\">" +
+            "&nbsp; &nbsp; &nbsp; 系 統 為 光 學 上 常 見 的 訊 號 處 理 系 統，系 統 為 就 是 將 輸 入 的<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "資 料 放 置 於 第 一 面 透 鏡 的 前 焦 平 面 上，並 在 後 焦 平 面 產 生 其 頻<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "譜，接 著 經 過 濾 波 後，再 由 第 二 面 透 鏡 再 做 一 次 傅 氏 轉 換 最 後 產<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "生 濾 波 後 的 圖 形 。 進 行 濾 波 時 就 在 此 頻 譜 面 遮 擋 不 要 的 頻 率，<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "遮 擋 高 頻、低 頻、x 方 向 的 頻 率、y 方 向 的 頻 率 皆 會 在 最 後 的 成 像<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "面 上 有 不 同 的 變 化。"
+            + "<br /><div style=\"width:100%;height:80px;\"></div>" +
+            "</div></html>");
+    public static javax.swing.JLabel desPhaseRetarder =
+            new JLabel("<html><div style=\"padding-left:6px;padding-right:5px;padding-top:5px;margin: 0 0.07em 0 -0.13em;background-color:white;width:100%;word-spacing:30px;font-family:MS Mincho;font-size:11px;font-weight: normal;\">" +
+            "&nbsp; &nbsp; &nbsp; 相 移 式 數 位 全 像 術 以 光 的 干 涉 原 理 為 基 礎，"
+            + "改 變 物 光 及 參<br /><div style=\"width:100%;height:3px;\"></div> "
+            + "考 光 的 相 對 相 位，通 常 是 利 用 壓 電 材 料 改 變 參 考 光 的 光 程 來 造<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "成 相 位 差 使 干 涉 條 紋 分 布 改 變，重 複 數 次 後 得 到 多 張 不 同 干 涉<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "條 紋，再 經 由 這 些 干 涉 圖 形 的 光 強 分 布 變 化 求得 原 物 波 的 相 位<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "分 布。得 到 了 物 波 的 光 強 及 相 位，我 們 即 能 利 用 近 場 繞 射 理 論 <br /><div style=\"width:100%;height:3px;\"></div>"
+            + "回 推 出 原 體。"
+            + "<br /><div style=\"width:100%;height:80px;\"></div>"
+             + "</div></html>");
+    public static javax.swing.JLabel desTalbotImage =
+                        new JLabel("<html><div style=\"padding-left:6px;padding-right:5px;padding-top:5px;margin: 0 0.07em 0 -0.13em;background-color:white;width:100%;word-spacing:30px;font-family:MS Mincho;font-size:11px;font-weight: normal;\">" +
+                        "&nbsp; &nbsp; &nbsp; 繞 射 理 論 中 的 近 場 繞 射 理 論 (Fresnel Diffraction) 有 著 相 當 複<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "雜 的 積 分 公 式, 一 般 積 分 的 結 果 可 由 方 孔 繞 射 看 出 是 相 當 龐 大<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "且 複 雜，但 週 期 性 光 柵 的 繞 射 卻 有 較 簡 單 的 結 果 及 特 殊 的 自 成<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "像 現 象 發 生. Talbot Images 為 週 期 性 光 柵 在 近 場 繞 射 時 所 產 生 的<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "自 成 像，其 產 生 的 位 置 與 光 柵 週 期 平 方 成 正 比，波 長 成 反 比， 並<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "在 其 整 數 倍 的 距 離 上 會 一 直 看 到 自 成 像，而 在 這 些 成 像 的 中 間<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "還 能 觀 察 到 與 原 影 像 對 比 相 反 的 自 成 像，實 驗 中 由 &nbsp;SLM&nbsp; 產 生 週 <br /><div style=\"width:100%;height:3px;\"></div>"
+            + "期 性 光 柵，並 由 CCD 拍 攝 所 產 生 的 自 成 像，改 變 CCD 的 位 置 觀 察 影<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "像 變 化 及 位 置。<br /><div style=\"width:100%;height:15px;\"></div>"
+            + "</div></html>");
+    public static javax.swing.JLabel desWavefront =
+            new JLabel("<html><div style=\"padding-left:6px;padding-right:5px;padding-top:5px;margin: 0 0.07em 0 -0.13em;background-color:white;width:100%;word-spacing:30px;font-family:MS Mincho;font-size:11px;font-weight: normal;\">" +
+            "&nbsp; &nbsp; SLM (Spatial Light Modulator) 能 接 受 電 子 訊 號 來 改 變 光 訊 號 電 子<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "訊 號 使 液 晶 分 子 排 列 方 向 偏 轉 改 變 液 晶 的 折 射 率， 使 得 通 過<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "SLM 的 光 能 有 不 同 的 相 位 延 遲 來 造 成 偏 振 態 的 改 變，再 配 合 上<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "偏 振 片 及 檢 偏 片 的 使 用 來 改 變 光 波 的 振 幅 或 相 位。調 制 出 合 適<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "的 光 訊 號 除了能 造 成 各 種 不 同 物 波 來 產 生 影 像 外，還 能 讓 SLM 擬<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "出 各 種 簡 單 的 波 形，如 球 面 波、圓 柱 波、及 各 種 不 同 方 向 傳 模 遞<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "的 平 面 波，而 這 些 波 形一般 都 是 由 透 鏡、圓 柱 透 鏡、平 面 鏡 來 產 生<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "所 以 能 製 造 出 這 些 波 形 也 代 表 著 SLM 能 當 作 可 調 變 的 光 學 元 件<br /><div style=\"width:100%;height:3px;\"></div>"
+            + "<br /><div style=\"width:100%;height:15px;\"></div>"
+            + "</div></html>");
+    public static javax.swing.JLabel desWavelength =
+            new JLabel("<html><b>No description available</b><br></html>");
+    public static javax.swing.JLabel desCalibration =
+            new JLabel("<html><b>No description available</b><br></html>");
+    public static javax.swing.JLabel  desImportfile =
+            new JLabel("<html><b>No description available</b><br></html>");
+}
