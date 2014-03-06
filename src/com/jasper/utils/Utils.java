946a0c85d9102ec5d6a569fdab64094aaf5aec38
sonnv <>
2014-03-06 16:14:37 +0700

    updated createDirectoryLogFileCGH method Utils class - WIN, LINUX

--- src/com/jasper/utils/Utils.java
+++ src/com/jasper/utils/Utils.java
@@ -103,31 +103,31 @@ public class Utils {
             fileDirectory = new File(Constant.FILE_PATH);
             fileDirectory.mkdir();
             
-            if(!checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH1)) {
+            if(checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH1)) {
                 fileCGH1 = new File(Constant.FILE_PATH, Constant.FILE_NAME_CGH1);
                 fileCGH1.createNewFile();
             } 
-            if(!checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH3)) {
+            if(checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH3)) {
                 fileCGH3 = new File(Constant.FILE_PATH, Constant.FILE_NAME_CGH3);
                 fileCGH3.createNewFile();
             }
-            if(!checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH4)) {
+            if(checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH4)) {
                 fileCGH4 = new File(Constant.FILE_PATH, Constant.FILE_NAME_CGH4);
                 fileCGH4.createNewFile();
             }
-            if(!checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH5)) {
+            if(checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH5)) {
                 fileCGH5 = new File(Constant.FILE_PATH, Constant.FILE_NAME_CGH5);
                 fileCGH5.createNewFile();
             }
-            if(!checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH6)) {
+            if(checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH6)) {
                 fileCGH6 = new File(Constant.FILE_PATH, Constant.FILE_NAME_CGH6);
                 fileCGH6.createNewFile();
             }
-            if(!checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH8)) {
+            if(checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH8)) {
                 fileCGH8 = new File(Constant.FILE_PATH, Constant.FILE_NAME_CGH8);
                 fileCGH8.createNewFile();
             }
-            if(!checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH10)) {
+            if(checkFileExists(Constant.FILE_PATH + File.separator + Constant.FILE_NAME_CGH10)) {
                 fileCGH10 = new File(Constant.FILE_PATH, Constant.FILE_NAME_CGH10);
                 fileCGH10.createNewFile();
             }
